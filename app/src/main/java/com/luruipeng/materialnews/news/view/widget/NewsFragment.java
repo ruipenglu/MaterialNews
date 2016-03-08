cb8290fdc1f6bf7fb6d74a74a5d72b028678ed1a
lugegege <luruipeng1994@foxmail.com>
2016-03-08 23:05:41 +0800

    change toolbar and tablayout

--- app/src/main/java/com/luruipeng/materialnews/news/view/widget/NewsFragment.java
+++ app/src/main/java/com/luruipeng/materialnews/news/view/widget/NewsFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
